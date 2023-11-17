d7a17e7c4055ee66b4d54a661988b9d0ddb734a3
hnthh <anyaagarenko@gmail.com>
2023-11-17 19:45:23 +0300

    configure `straight.el`, remove `melpa`

--- init.el
+++ init.el
@@ -1,7 +1,21 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+;; straight.el
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+
+  (load bootstrap-file nil 'nomessage))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
