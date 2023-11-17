beb242a585363b2f8c4e656c181948a6119c0319
Anya Agarenko <anyaagarenko@gmail.com>
2023-11-17 20:09:22 +0300

    add `straight`, `copilot` (#1)

--- init.el
+++ init.el
@@ -1,7 +1,35 @@
 (require 'package)
 
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+;; straight.el
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+
+;; copilot
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :ensure t)
+
+(add-hook 'post-command-hook #'copilot-clear-overlay)
+(add-hook 'prog-mode-hook 'copilot-mode)
+
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion-by-word)
+(define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
