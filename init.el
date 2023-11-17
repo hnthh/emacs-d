15c25db72a0a4fecb74cd4ebc0abc8dd8ff5b892
hnthh <anyaagarenko@gmail.com>
2023-11-17 20:06:35 +0300

    configure `copilot`, bind `straight` to `'use-package`

--- init.el
+++ init.el
@@ -17,6 +17,20 @@
 
   (load bootstrap-file nil 'nomessage))
 
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+
+;; copilot
+(use-package copilot
+  :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
+  :ensure t)
+
+(add-hook 'post-command-hook #'copilot-clear-overlay)
+(add-hook 'prog-mode-hook 'copilot-mode)
+
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion-by-word)
+(define-key copilot-completion-map (kbd "C-<tab>") 'copilot-accept-completion)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
